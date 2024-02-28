713df829c4bdf6c53940dd983df8690081d05a90
Dezodemius <gladkovyegor@gmail.com>
2024-02-28 23:58:37 +0400

    game proto

--- scenes/main/Main.cs
+++ scenes/main/Main.cs
@@ -24,4 +24,40 @@ public partial class Main : Node
 
 		GetNode<Timer>("StartTimer").Start();
 	}
+
+	private void OnMobTimerTimeout()
+	{
+		Mob mob = MobScene.Instantiate<Mob>();
+
+		var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+		mob.Position = mobSpawnLocation.Position;
+
+		direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+		mob.Rotation = direction;
+
+		var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+		mob.LinearVelocity = velocity.Rotated(direction);
+
+		AddChild(mob);
+	}
+
+	public override void _Ready()
+	{
+		NewGame();
+	}
+
+	private void OnScoreTimerTimeout()
+	{
+		_score++;
+	}
+
+	private void OnStartTimerTimeout()
+	{
+		GetNode<Timer>("MobTimer").Start();
+		GetNode<Timer>("ScoreTimer").Start();
+	}
 }
